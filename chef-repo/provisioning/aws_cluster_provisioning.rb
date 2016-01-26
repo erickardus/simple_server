375c2e9126e263fa26645cab9fc207d45289cf23
erickardus <erickardus@gmail.com>
2016-01-26 16:54:43 -0600

    Fix for django 1.9 upgrade

--- chef-repo/provisioning/aws_cluster_provisioning.rb
+++ chef-repo/provisioning/aws_cluster_provisioning.rb
@@ -1,12 +1,12 @@
 require 'chef/provisioning'
 require 'chef/provisioning/aws_driver'
 
-with_driver "aws::us-east-1"
+with_driver "aws::us-west-1"
 
 with_machine_options({
     ssh_username: "ec2-user",
     bootstrap_options: {
-        image_id: "ami-0d4cfd66",
+        image_id: "ami-353aff71",
         instance_type: "t2.micro",
     },
 })
@@ -17,11 +17,9 @@ with_chef_server "https://manage.chef.io/organizations/simple_server",
 
 
 machine_batch "cluster" do
-    1.upto(2) do |i|
-        machine "hello#{i}" do
+    1.upto(1) do |i|
+        machine "server#{i}" do
             
-            
-                role 'webserver'
             
         end
     end
