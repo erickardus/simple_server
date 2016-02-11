d98e5e3d6e4db6126aae398cbe7c768fc4812843
erickardus <erickardus@gmail.com>
2016-02-11 10:11:17 -0600

    am

--- simple-server-project/simple_server/azure_cluster_create/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AzureClusterCreateConfig(AppConfig):
-    name = 'azure_cluster_create'
--- simple-server-project/simple_server/cluster_create/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def cluster_creator(request):
-    """Starting point for cluster creation.
-    Allows you to pick different cloud providers.
-    """
-    return render(request, 'cluster_create.html')
